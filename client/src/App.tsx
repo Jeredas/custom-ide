c5531992619ae1ad0ca05b958ae0d4ad95d9fabd
Uladzimir Kamarou <u.kamarou@softteco.com>
2022-03-11 10:54:01 +0300

    client fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 import AceEditor from "react-ace";
 import "ace-builds/webpack-resolver";
 import "ace-builds/src-noconflict/mode-c_cpp";
@@ -8,91 +8,92 @@ import "ace-builds/src-noconflict/mode-javascript";
 import "ace-builds/src-noconflict/mode-java";
 import "ace-builds/src-noconflict/mode-python";
 import "ace-builds/src-noconflict/theme-monokai";
-import "ace-builds/src-noconflict/ext-language_tools"
+import "ace-builds/src-noconflict/ext-language_tools";
 
 function App() {
   const [value, setValue] = useState(template);
-  const [language,setLanguage] = useState('java');
-  const [device,setDevice] = useState({});
-  
-const validateCode = (value: string) => {
-  console.log("validate");
-};
+  const [language, setLanguage] = useState("java");
+  const [device, setDevice] = useState({});
 
-const handleSelect = (e:any) => {
-  setLanguage(e.target.value);
+  const validateCode = (value: string) => {
+    console.log("validate");
+  };
+
+  const handleSelect = (e: any) => {
+    setLanguage(e.target.value);
+  };
+
+  const handleSave = async () => {
+    let response = await fetch("http://localhost:6600/savefile", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify({ code: value }),
+    });
+  };
+  return (
+    <div className="main_wrapper">
+      <select onChange={(e) => handleSelect(e)} className='select'>
+        <option value="javascript">javascript</option>
+        <option value="c_cpp">C++</option>
+        <option value="python">Python</option>
+      </select>
+      <AceEditor
+        placeholder="Placeholder Text"
+        mode={language}
+        theme="monokai"
+        name="blah2"
+        commands={[
+          {
+            // commands is array of key bindings.
+            name: "commandName", //name for the key binding.
+            bindKey: { win: "Ctrl-k", mac: "Command-k" }, //key combination used for the command.
+            exec: () => validateCode(value), //function to execute when keys are pressed.
+          },
+        ]}
+        onChange={(e) => {
+          setValue(e);
+        }}
+        width={`1000px`}
+        height={`1000px`}
+        fontSize={14}
+        showPrintMargin={true}
+        showGutter={true}
+        highlightActiveLine={true}
+        value={value}
+        setOptions={{
+          useWorker: true,
+          enableBasicAutocompletion: true,
+          enableLiveAutocompletion: true,
+          enableSnippets: true,
+          showLineNumbers: true,
+          tabSize: 4,
+        }}
+      />
+      <button
+        className="button"
+        onClick={() => {
+          navigator.bluetooth
+            .requestDevice({
+              acceptAllDevices: true,
+            })
+            .then((data) => setDevice(data));
+        }}
+      >
+        View devices
+      </button>
+      <button
+        className="button"
+        onClick={() => {
+          handleSave();
+        }}
+      >
+        Save sketch
+      </button>
+    </div>
+  );
 }
-
-
-const handleSave = async() => {
-  let response = await fetch('http://localhost:6600/savefile', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8'
-    },
-    body: JSON.stringify({code:value})
-  });
-}
-return (
-  <>
-    <select onChange={(e) => handleSelect(e)}>
-      <option value="javascript">javascript</option>
-      <option value="c_cpp">C++</option>
-      <option value="python">Python</option>
-    </select>
-    <AceEditor
-      placeholder="Placeholder Text"
-      mode={language}
-      theme="monokai"
-      name="blah2"
-      commands={[
-        {
-          // commands is array of key bindings.
-          name: "commandName", //name for the key binding.
-          bindKey: { win: "Ctrl-k", mac: "Command-k" }, //key combination used for the command.
-          exec: () => validateCode(value), //function to execute when keys are pressed.
-        },
-      ]}
-      onChange={(e) => {
-        setValue(e);
-      }}
-      width={`1000px`}
-      height={`1000px`}
-      fontSize={14}
-      showPrintMargin={true}
-      showGutter={true}
-      highlightActiveLine={true}
-      value={value}
-      setOptions={{
-        useWorker: true,
-        enableBasicAutocompletion: true,
-        enableLiveAutocompletion: true,
-        enableSnippets: true,
-        showLineNumbers: true,
-        tabSize: 4,
-      }}
-    />
-    <button
-      onClick={() => {
-        navigator.bluetooth
-          .requestDevice({
-            acceptAllDevices: true,
-          })
-          .then((data) => setDevice(data));
-      }}
-    >
-      View devices
-    </button>
-    <button
-      onClick={() => {
-        handleSave();
-      }}
-    >
-      save sketch
-    </button>
-  </>
-);
-    }
 
 export default App;
 const template = `
@@ -109,4 +110,4 @@ void loop() {
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
 }
-`
+`;
